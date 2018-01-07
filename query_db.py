053fc815930528a6d41c67ad081bde4e7040dd1f
Ralf Kotulla <ralf.kotulla@gmail.com>
2018-01-07 05:56:49 -0600

    cleaned up database query code and re-packed as class.

--- query_db.py
+++ query_db.py
@@ -8,41 +8,48 @@ from pprint import pprint
 
 import config
 
+class ODIDB(object):
 
+    def __init__(self):
 
-def query_exposures_for_transfer():
+        dsn = cx_Oracle.makedsn(host='odiserv.kpno.noao.edu',
+                                port=1521, sid='odiprod')
+        # print(dsn)
 
-    dsn = cx_Oracle.makedsn(host='odiserv.kpno.noao.edu',
-                            port=1521, sid='odiprod')
-    print(dsn)
+        # establish connection to Oracle-DB
+        # connection = odidb.connect('odi@//odiserv.kpno.noao.edu:1521/odiprod/')
+        self.connection = cx_Oracle.connect(config.oracle_user, config.oracle_password, dsn)
 
-    # establish connection to Oracle-DB
-    # connection = odidb.connect('odi@//odiserv.kpno.noao.edu:1521/odiprod/')
-    connection = cx_Oracle.connect(config.oracle_user, config.oracle_password, dsn)
+        self.cursor = self.connection.cursor()
 
-    cursor = connection.cursor()
+    def query_exposures_for_transfer(self):
 
-    # select all exposures that have not been marked as complete (return code=0) yet
-    sql = """\
-SELECT  exp.id,exp.exposure,exp.fileaddr
-FROM    EXPOSURES exp
-WHERE   exp.id NOT IN
-(
-SELECT  expid
-FROM    EXPOSURE_EVENT
-WHERE event like '%:: 0%'
-)
-"""
+        # select all exposures that have not been marked as complete (return code=0) yet
+        sql = """\
+    SELECT  exp.id,exp.exposure,exp.fileaddr
+    FROM    EXPOSURES exp
+    WHERE   exp.id NOT IN
+    (
+    SELECT  expid
+    FROM    EXPOSURE_EVENT
+    WHERE event like '%:: 0%'
+    )
+    """
 
-    cursor.execute(sql)
-    results = cursor.fetchall()
-    pprint(results)
+        self.cursor.execute(sql)
+        results = self.cursor.fetchall()
+        # pprint(results)
+        return results
 
-    cursor.close()
-    connection.close()
+    def __del__(self):
+        self.cursor.close()
+        self.connection.close()
 
 
 
 if __name__ == "__main__":
 
-    query_exposures_for_transfer()
+    db = ODIDB()
+
+    exposures = db.query_exposures_for_transfer()
+    print("Found %d exposures to be transferred" % (len(exposures)))
