6913b8c63cb7716de4e75e1d4076cc829da00b30
Ralf Kotulla <ralf.kotulla@gmail.com>
2018-01-08 14:59:28 -0600

    added md5.txt file to tar-ball as required by PPA.

--- dts.py
+++ dts.py
@@ -44,6 +44,8 @@ class DTS ( object ):
         if (self.obsid is None):
             self.update_obsid_from_files()
 
+        self.archive_ingestion_message = None
+
         self.database = database
         if (auto_start):
             self.archive()
@@ -58,13 +60,14 @@ class DTS ( object ):
                 for ext in hdulist:
                     if ('OBSID' in ext.header):
                         self.obsid = ext.header['OBSID']
-                        print("FOUND OBSID: %s" % (self.obsid))
+                        #print("FOUND OBSID: %s" % (self.obsid))
                         return
         return
 
     def archive(self):
         self.make_tar()
         self.transfer_to_archive()
+        self.report_new_file_to_archive()
         self.register_transfer_complete()
 
     def get_filelist(self):
@@ -75,13 +78,23 @@ class DTS ( object ):
         self.filelist = fits_files
 
     def make_tar(self):
-        # print("Making tar ball")
 
         # run fpack to enable compression on all image files
+        print("Compressing data")
+        md5_data = []
         for filename in self.filelist: #fits_files:
-            self.fpack(filename)
+            fz_file, md5 = self.fpack(filename)
+            md5_data.append("%s %s" % (md5, fz_file))
+
+        # create the md5.txt file
+        md5_filename = os.path.join(os.path.join(self.scratch_dir, self.dir_name),
+                                    "md5.txt")
+        print(md5_data)
+        with open(md5_filename, "w") as md5f:
+            md5f.write("\n".join(md5_data))
 
         # Now create the actual tar ball
+        print("Making tar ball")
         tar_cmd = "tar --create --seek --file=%s --directory=%s %s" % (
             self.tar_filename, self.scratch_dir, self.dir_name)
         # print(tar_cmd)
@@ -98,10 +111,12 @@ class DTS ( object ):
     def fpack(self, filename):
         _, basename = os.path.split(filename)
         fz_filename = basename+".fz"
-        cmd = "fpack -S %s > %s" % (filename, os.path.join(self.tar_directory, fz_filename))
+        fz_filename_full = os.path.join(self.tar_directory, fz_filename)
+        cmd = "fpack -S %s > %s" % (filename, fz_filename_full)
         # print(cmd)
         self.execute(cmd)
-        return
+        checksum = self.calculate_checksum(fz_filename_full)
+        return fz_filename, checksum
 
     def transfer_to_archive(self):
 
@@ -112,8 +127,8 @@ class DTS ( object ):
         else:
             raise ValueError("Could not identfy which transfer protocal to use")
 
-        # print("Copying to archive")
-        print(cmd)
+        print("Copying to archive using %s (%s)" % (self.transfer_protocol, cmd))
+        # print(cmd)
         start_time = time.time()
         self.execute(cmd)
         end_time = time.time()
@@ -123,9 +138,10 @@ class DTS ( object ):
 
     def register_transfer_complete(self):
         # print("Marking as complete")
-        event = "pyDTS: fpack(%d) - tar(%.1fMB, MD5=%s) - transfer(%.1fs @ %.2fMB/s) - ingest: OK :: -1" % (
+        event = "pyDTS: fpack(%d) - tar(%5.1fMB, MD5=%s) - transfer(%4.1fs @ %5.2fMB/s via %s) - ingest(%s): OK :: -1" % (
             len(self.filelist), self.tar_filesize/2**20, self.tar_checksum,
-            self.tar_transfer_time, self.tar_filesize/2**20/self.tar_transfer_time
+            self.tar_transfer_time, self.tar_filesize/2**20/self.tar_transfer_time,
+            self.transfer_protocol, self.archive_ingestion_message,
         )
         self.database.mark_exposure_archived(self.obsid, event=event)
         pass
@@ -154,3 +170,6 @@ class DTS ( object ):
 
         # [(fname, hash_bytestr_iter(file_as_blockiter(open(fname, 'rb')), hashlib.md5()))
         #  for fname in fnamelst]
+
+    def report_new_file_to_archive(self):
+        return
