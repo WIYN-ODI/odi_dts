06f024a5992e4d76414590884ec11137a2c6f064
Ralf Kotulla <ralf.kotulla@gmail.com>
2018-01-08 04:44:30 -0600

    added some multi-threading functionality to the main routine.

--- odi_dts.py
+++ odi_dts.py
@@ -5,12 +5,37 @@ import sys
 import subprocess
 
 import argparse
+import threading
+import queue
 
 import dts
 import query_db
 
-def get_obsid_from_directory(dirname):
-    return None
+
+class DTS_Thread(threading.Thread):
+    """Threaded Url Grab"""
+    def __init__(self, queue, out_queue=None, database=None):
+        threading.Thread.__init__(self)
+        self.queue = queue
+        self.out_queue = out_queue
+        self.database = database
+
+    def run(self):
+        while(True):
+            #grabs host from queue
+            try:
+                exposure_info = self.queue.get()
+            except queue.Empty as e:
+                break
+            if (exposure_info is None):
+                break
+
+            (dir, obsid) = exposure_info
+            exposure2archive = dts.DTS(dir, obsid=obsid, database=self.database)
+
+            #signals to queue job is done
+            self.queue.task_done()
+
 
 if __name__ == "__main__":
 
@@ -35,16 +60,31 @@ if __name__ == "__main__":
         input_dirs = [(dir, obsid) for (id,obsid,dir) in exposures]
     else:
         input_dirs = []
-        for dir in args.inputdir:
-            obsid = get_obsid_from_directory(dir)
-            input_dirs.append((dir,obsid))
-        # input_dirs = args.inputdir
+        # for dir in args.inputdir:
+        #     obsid = get_obsid_from_directory(dir)
+        #     input_dirs.append((dir,obsid))
+        input_dirs = [(dir,None) for dir in args.inputdir]
 
+    # create a work-queue and populate it with exposures to archive
+    dts_queue = queue.Queue()
     for (dir, obsid) in input_dirs:
         print(dir, obsid)
-        odidb.mark_exposure_archived(obsid)
-        break
+
+        dts_queue.put((dir, obsid))
+        # object = dts.DTS(dir, obsid=obsid, database=odidb)
+        # odidb.mark_exposure_archived(obsid)
+        #break
 
     print(len(input_dirs))
         #transfer = dts.DTS(dir)
 
+
+    #
+    # start the worker threads
+    #
+    for i in range(args.nthreads):
+        t = DTS_Thread(queue=dts_queue, database=odidb)
+        t.setDaemon(True)
+        t.start()
+
+    dts_queue.join()
