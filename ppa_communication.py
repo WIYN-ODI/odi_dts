df46a87a651cfb9fdcf1a90424cbf15a161bcbb8
Ralf Kotulla <kotulla@wisc.edu>
2018-01-18 10:52:40 -0600

    added comment field to PPA-bound message Fixed communication to connect to PPA for each exposure and disconnect afterwards to avoid timeouts and related problems.

--- ppa_communication.py
+++ ppa_communication.py
@@ -8,11 +8,14 @@ import json
 import datetime
 import logging
 
+import listener
+
 logging.getLogger("pika").setLevel(logging.WARNING)
 
-class PPA (object):
+class PPA (object): #listener.Listener):
 
     def __init__(self, server_name=None, username=None, password=None, virtual_host=None, port=5672, reuse=True):
+        self.logger = logging.getLogger("PPAcomm")
 
         self.server_name = server_name if server_name is not None else config.ppa_server_name
         self.username = username if username is not None else config.ppa_username
@@ -30,13 +33,15 @@ class PPA (object):
             virtual_host=self.virtual_host,
             credentials=self.credentials,
         )
+        #listener.Listener.__init__(self, self.parameters)
         self.connection = None
 
         # if (reuse):
         #     self.channel = None
         #     self.queue = None
 
-        self.connect()
+        #self.connect()
+        #self._channel = self._connection.channel()
 
     def connect(self):
         if (self.connection is None):
@@ -58,13 +63,10 @@ class PPA (object):
 
 
 
-    def _on_connect(self):
-        return
-
 
     def send_message(self, target_queue, message):
-        if (not self.connect()):
-            return False
+
+        self.connect()
 
         queue = self.channel.queue_declare(queue=target_queue, durable=True)
 
@@ -74,13 +76,21 @@ class PPA (object):
             body=message,
             properties=pika.BasicProperties(content_type='text/plain', delivery_mode=1)
         )
-        # print(success)
+
+        self.close()
+
+        # print(succesS)
         return success
 
         # channel.basic_publish()
 
 
-    def report_exposure(self, timestamp, obsid, msg_type):
+    def report_exposure(self, obsid, msg_type, timestamp=None, comment=None):
+
+        if (timestamp is None):
+            timestamp = datetime.datetime.now()
+        if (comment is None):
+            comment = ""
 
         msg = json.dumps({
             'time': str(timestamp),
@@ -88,10 +98,24 @@ class PPA (object):
             'source': 'instrument',
             'destination': 'ppa',
             'type': msg_type,
+            'comment': comment
         })
 
         return self.send_message("instrument.ralftest", msg)
 
+
+    def close(self):
+        #self.logger.debug("Closing connection to PPA")
+        if (self.channel is not None):
+            self.channel.close()
+            self.channel = None
+
+        if (self.connection is not None):
+            self.connection.close()
+            self.connection = None
+
+    def __del__(self):
+        self.close()
 
 if __name__ == "__main__":
 
