4f6a28ae34e38cbba7cdaa64c971da4503ec1c44
Ralf Kotulla <kotulla@wisc.edu>
2018-02-19 12:07:46 -0600

    added config example file.

---
+++ config.py.example.py
@@ -0,0 +1,15 @@
+
+oracle_user = 'user'
+oracle_password = 'pass'
+oracle_server = '//host:port/dbname'
+
+remote_server = "user@server"
+remote_directory = "/some/dir/"
+
+tar_scratchdir = "/tmp"
+
+ppa_server_name = "example.com"
+ppa_username = "user"
+ppa_password = "pass"
+ppa_port = 12345
+ppa_virtualhost = "vhost"
