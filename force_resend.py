ec6a5d9d50239773245c7f481128d6fc3c829099
Ralf Kotulla @ odidb <ralf.kotulla@gmail.com>
2025-02-04 16:34:38 -0700

    some bugfixes

--- force_resend.py
+++ force_resend.py
@@ -15,22 +15,21 @@ if __name__ == "__main__":
     args = commandline.parse()
     print(args.inputdir)
 
-    db = ODIDB()
-
     # get OBSIDs for all input files
     obsids = []
-    for fn in args.input_dir:
+    for fn in args.inputdir:
         if (os.path.isdir(fn)):
             dirlist = glob.glob(fn + "/*.fits")
+            print("Input was directory, checking files: %s" % (", ".join(dirlist)))
             for _fn in dirlist:
                 hdulist = pyfits.open(_fn)
-            try:
-                obsid = hdulist[0].header['OBSID']
-                print("Found OBSID %s in %s" % (obsid, _fn))
-                obsids.append(obsid)
-                break
-            except KeyError:
-                    continue
+                try:
+                    obsid = hdulist[0].header['OBSID']
+                    print("Found OBSID %s in %s" % (obsid, _fn))
+                    obsids.append(obsid)
+                    break
+                except KeyError:
+                        continue
         elif (os.path.isfile(fn)):
             try:
                 hdulist = pyfits.open(fn)
@@ -42,7 +41,10 @@ if __name__ == "__main__":
         else:
             print("Not sure what to do with %s" % (fn))
 
-    print("Adding resend requests for %s" % ("\ ** ".join([]+obsids)))
+    print("Adding resend requests for %s" % ("\n ** ".join(['']+obsids)))
+
+    db = ODIDB()
+
 
     #
     # # get EXPID for
